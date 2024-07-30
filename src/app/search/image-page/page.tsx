7657cb26352a635077106dd51d14c7b24d5dc279
Daniil-l17 <mlumed221@gmail.com>
2024-07-30 16:39:58 +0500

    добавил Suspense к странице image-page

--- src/app/search/image-page/page.tsx
+++ src/app/search/image-page/page.tsx
@@ -1,10 +1,15 @@
 import { Metadata } from 'next';
 import ImagePage from './ImagePage';
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
 	title: 'Firefox developer image'
 };
 
 export default function page() {
-	return <ImagePage />;
+	return (
+		<Suspense>
+			<ImagePage />
+		</Suspense>
+	);
 }
